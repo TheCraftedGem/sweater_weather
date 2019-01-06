3c37e48c80b05513fd0542908a5adb85ed74659f
Emilio <thecraftedgem@tuta.io>
2019-01-06 16:48:40 -0700

    back to 100% test coverage

--- spec/requests/api/v1/new_user_request_spec.rb
+++ spec/requests/api/v1/new_user_request_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'POST /api/v1/users' do
   it 'creates a user and returns an API key' do
-    
-    parameters = "?email=whatever@example.com&password=password&password_confirmation=password"
 
-    post "/api/v1/users#{parameters}"
+    params = { email: 'whatever@example.com', password: 'password', password_confirmation: 'password'}
+
+    post "/api/v1/users?email=#{params[:email]}&password=#{params[:password]}&password_confirmation=#{params[:password_confirmation]}"
 
     expect(response).to be_successful
 
@@ -13,5 +13,19 @@ RSpec.describe 'POST /api/v1/users' do
     expect(results[:data]).to have_key(:type)
     expect(results[:data][:attributes]).to have_key(:api_key)
     expect(results[:data][:attributes][:api_key]).to be_a String
+    expect(User.all.count).to eq(1)
+  end
+    it 'fails if passwords do not match' do
+    params = {
+      email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "potatoe323"
+    }
+
+    post "/api/v1/users?email=#{params[:email]}&password=#{params[:password]}&password_confirmation=#{params[:password_confirmation]}"
+
+    expect(response.status).to eq(422)
+    expect(response.body).to eq("Woops Try Again")
+    expect(User.all.count).to eq(0)
   end
 end
