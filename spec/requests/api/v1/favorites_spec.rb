afb3516f078f879a723f1f1cc11de0cfb30c7ebf
Emilio <thecraftedgem@tuta.io>
2019-01-07 01:20:54 -0700

    working on deleting favorite location

--- spec/requests/api/v1/favorites_spec.rb
+++ spec/requests/api/v1/favorites_spec.rb
@@ -48,4 +48,20 @@ RSpec.describe 'POST /api/v1/users' do
       expect(results[:data][0][:attributes]).to have_key(:current_weather)
       expect(results[:data][0][:attributes]).to have_key(:location)
     end
+
+    it 'can delete a favorited location' do
+      user = create(:user, email: 'whatever@example.com', password: "password" )
+      user.favorites.create(location: 'Denver, co')
+      user.favorites.create(location: 'Golden, co')
+
+      params = {location: "Denver, co", api_key: user.api_key}
+      delete "/api/v1/favorites?location=#{params[:location]}&api_key=#{params[:api_key]}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      results = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(user.favorites.count).to eq(1)
+    end
 end
